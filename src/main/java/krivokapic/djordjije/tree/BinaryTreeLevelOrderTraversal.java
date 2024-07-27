69f2ef140ddb1d656d9ba107698488ecfcbb3d87
Djordjije Krivokapic <djolek@amazon.com>
2024-07-27 13:26:12 +0200

    Binary Trees Problem Solving

---
+++ src/main/java/krivokapic/djordjije/tree/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,48 @@
+package krivokapic.djordjije.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+public class BinaryTreeLevelOrderTraversal {
+
+    // https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return List.of();
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+
+                level.add(node.val);
+            }
+
+            result.add(level);
+        }
+
+        return result;
+    }
+
+}
---
+++ src/main/java/krivokapic/djordjije/tree/SameTree.java
@@ -0,0 +1,20 @@
+package krivokapic.djordjije.tree;
+
+public class SameTree {
+
+    // https://leetcode.com/problems/same-tree/description/
+    // Time Complexity: O(n)
+    // Space Complexity: O(h)
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+
+        return false;
+    }
+
+}
