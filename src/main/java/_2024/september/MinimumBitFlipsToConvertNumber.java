cd51b67a677abb10764521a4def65288ea70e012
Djordjije Krivokapic <djolek@amazon.com>
2024-09-14 18:19:56 +0200

    Daily Leetcode

---
+++ src/main/java/_2024/september/MinimumBitFlipsToConvertNumber.java
@@ -0,0 +1,17 @@
+package _2024.september;
+
+public class MinimumBitFlipsToConvertNumber {
+    // https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
+    // Time Complexity: O(1)
+    // Space Complexity: O(1)
+    public int minBitFlips(int start, int goal) {
+        String result = Integer.toBinaryString(start ^ goal);
+        int bitFlips = 0;
+
+        for (int i = 0; i < result.length(); i++) {
+            bitFlips += result.charAt(i) == '1' ? 1 : 0;
+        }
+
+        return bitFlips;
+    }
+}
