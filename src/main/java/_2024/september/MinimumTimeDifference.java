72f1067033fdd5c00d5f52e141d7037ae30f7c0b
Djordjije Krivokapic <djolek@amazon.com>
2024-09-16 22:00:59 +0200

    Daily Leetcode

---
+++ src/main/java/_2024/september/MinimumTimeDifference.java
@@ -0,0 +1,63 @@
+package _2024.september;
+
+import java.util.List;
+
+
+public class MinimumTimeDifference {
+    // https://leetcode.com/problems/minimum-time-difference/
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    public int findMinDifference(List<String> timePoints) {
+        // O(1) space complexity
+        boolean[] minutes = new boolean[24 * 60];
+
+        // O(n) time complexity
+        for (String time : timePoints) {
+            int minutesTime = 60 * Integer.parseInt(time.substring(0, 2)) + Integer.parseInt(time.substring(3));
+            if (minutes[minutesTime]) {
+                return 0;
+            }
+
+            minutes[minutesTime] = true;
+        }
+
+        int minimumTimeDifference = Integer.MAX_VALUE;
+        int leftPointer = -1;
+        int rightPointer = -1;
+        int firstPointer = -1;
+
+        // O(1) time complexity
+        int index = 0;
+        while (index < minutes.length) {
+            if (!minutes[index]) {
+                index += 1;
+                continue;
+            }
+
+            leftPointer = index;
+            if (firstPointer == -1) {
+                firstPointer = index;
+            }
+
+            do {
+                index += 1;
+            } while (index < minutes.length && !minutes[index]);
+
+            if (index == minutes.length) {
+                break;
+            } else {
+                rightPointer = index;
+            }
+
+            minimumTimeDifference = Math.min(minimumTimeDifference, rightPointer - leftPointer);
+        }
+
+        return Math.min(minimumTimeDifference, 1440 - leftPointer + firstPointer);
+    }
+
+
+    public static void main(String[] args) {
+        MinimumTimeDifference minimumTimeDifference = new MinimumTimeDifference();
+        minimumTimeDifference.findMinDifference(List.of("23:59", "00:00"));
+    }
+}
